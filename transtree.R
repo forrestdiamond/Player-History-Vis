34b289378a3f23d7a50458a32b6f7dcc55519686
forrestdiamond <forrestdiamond@users.noreply.github.com>
2016-03-19 17:31:11 -0400

    update transtree
    comment out interactive mode (can't use with rshiny)

--- transtree.R
+++ transtree.R
@@ -4,14 +4,15 @@ transtree <- function(bbrefid){
 # This script will create a transaction tree for a baseball player
 
 # read in your retrosheet transaction csv (found here: http://www.retrosheet.org/transactions/)
-dat <- read.csv("../tran.csv", stringsAsFactors = FALSE)
+dat <- read.csv("./tran.csv", stringsAsFactors = FALSE)
 
 # alright, we want to get the player we're interested in first
 ## let's set it as David Aardsmaa to start, we'll make it interactive later
 # bbrefid <- "aardd001"
 
+#Can uncomment this section to do an interactive entry w/o rshiny
 # n <- readline(prompt="Enter a bbrefid: ")
-# test <- TRUE
+#test <- TRUE
 # while(test){
 #   n <- readline(prompt="Not a valid bbrefid, try again: ")
 #   if(length(grep(paste("^",n,"$",sep=""), dat$BBREFID) > .5))test <- FALSE
